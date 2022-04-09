b861690dcb26a83035c0b5bde88008b86f5caf6e
Brandon Dees <dees@pm.me>
2022-04-09 15:58:15 -0600

    health check routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  health_check_routes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
